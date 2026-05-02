e2e400d81b2c78eaab5945b85d7086e7b93f66b1
FairyLlama <FairyLlama@users.noreply.github.com>
2026-05-02 21:23:17 +0200

    auth controller

--- AuthenticationService/obj/Debug/net10.0/AuthenticationService.AssemblyInfo.cs
+++ AuthenticationService/obj/Debug/net10.0/AuthenticationService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthenticationService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c06e69a856d7c4efbcbbc01b3a9093e6a7232ca5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3356ec857435a23696b01cad23d6342cb824af2")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthenticationService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthenticationService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
